3a1719f3cb0a0226317d1c1b8c1d2e416079772a
zhumengli <zhumengli@1078220816@qq.com>
2020-06-29 09:39:29 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,11 +9,14 @@ $(function () {
           layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function (index) {
                //do something
                // 1. 清空本地存储中的 token
-               localStorage.removeItem('token')
+               // localStorage.removeItem('token')
+               localStorage.clear()
                // 2. 重新跳转到登录页面
                location.href = './login.html'
                // 关闭 confirm 询问框
                layer.close(index);
+          }, function () {
+               console.log('cance1');
           });
      })
 })
